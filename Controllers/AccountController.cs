d1dd7ea9b6d92c9021e5f0f4f7dcb122eab30322
Filippa Fast <filippa.fast@learnet.se>
2023-10-12 11:32:17 +0200

    scss folder added

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Project_Course_Submission.Controllers
 {
 	public class AccountController : Controller
 	{
+		[Authorize]
 		public IActionResult Index()
 		{
 			return View();
