0dab67a6d59af83b0e97b896bc1caa757df2bdcc
Filippa Fast <filippa.fast@learnet.se>
2023-10-12 11:19:48 +0200

    Adding Products, login, logout controller

---
+++ Controllers/LogoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Course_Submission.Controllers
+{
+	public class LogoutController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Course_Submission.Controllers
+{
+	public class ProductsController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
