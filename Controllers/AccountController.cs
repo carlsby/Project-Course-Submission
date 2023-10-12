5b19a6b256b267e92446802dcd490229794f2fdd
Filippa Fast <filippa.fast@learnet.se>
2023-10-12 11:21:10 +0200

    Adding Account and RegisterController

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Course_Submission.Controllers
+{
+	public class AccountController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
