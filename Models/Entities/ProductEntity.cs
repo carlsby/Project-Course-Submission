b4354e114a1864290668b9e09c9182d0ffb56fc1
Filippa Fast <filippa.fast@learnet.se>
2023-10-13 11:06:07 +0200

    Updated ProductEntity

--- Models/Entities/ProductEntity.cs
+++ Models/Entities/ProductEntity.cs
@@ -8,9 +8,13 @@ namespace Project_Course_Submission.Models.Entities
 		[Key]
 		public string ArticleNumber { get; set; } = null!;
 		public string? ProductName { get; set; }
-		
+		public string Color { get; set; } = null!;
+		public string? Size { get; set; }
+
 		[Column(TypeName = "money")]
 		public decimal Price { get; set; }
+		
+		public string? Image { get; set; }	
 		public string? Description { get; set; }
 	}
 }
