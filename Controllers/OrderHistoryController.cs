ce5c4c5df29fdbbbdadf5f3278de3cf85ad09be5
Filip Sandwall <filip.sandwall@hotmail.com>
2023-10-17 01:48:46 +0200

    Orderhistory tillagt! temp link i nav.

---
+++ Controllers/OrderHistoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Course_Submission.Controllers
+{
+    public class OrderHistoryController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult History()
+        {
+            // Your action logic here
+            return View("_History"); // Assumes "_History.cshtml" is located in the Views folder
+        }
+    }
+}
