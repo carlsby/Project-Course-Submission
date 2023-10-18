b603edb3aa199ac3fab60a34efdb530ac2348915
Filippa Fast <filippa.fast@learnet.se>
2023-10-18 11:49:39 +0200

    Best Sellers items title

--- Services/BestSellersService.cs
+++ Services/BestSellersService.cs
@@ -9,13 +9,13 @@ namespace Project_Course_Submission.Services
             Title = "Best Sellers",
             BestItems = new List<BestSellersItemViewModel>
             { 
-                new BestSellersItemViewModel {Id = "1", Title = "Apple watch series", Price = 30, ImageUrl = "images/placeholders/270x295.svg"},
-				new BestSellersItemViewModel { Id = "2", Title = "Apple watch collection", Price = 30, ImageUrl = "images/placeholders/270x295.svg" },
-				new BestSellersItemViewModel { Id = "3", Title = "Apple watch collection", Price = 30, ImageUrl = "images/placeholders/270x295.svg" },
-				new BestSellersItemViewModel { Id = "4", Title = "Apple watch collection", Price = 30, ImageUrl = "images/placeholders/270x295.svg" },
-				new BestSellersItemViewModel { Id = "5", Title = "Apple watch collection", Price = 30, ImageUrl = "images/placeholders/270x295.svg" },
-				new BestSellersItemViewModel { Id = "6", Title = "Apple watch collection", Price = 30, ImageUrl = "images/placeholders/270x295.svg" },
-				new BestSellersItemViewModel { Id = "7", Title = "Apple watch collection", Price = 30, ImageUrl = "images/placeholders/270x295.svg" }
+                new BestSellersItemViewModel {Id = "1", Title = "Soft Chill Pants", Price = 30, ImageUrl = "images/placeholders/270x295.svg"},
+				new BestSellersItemViewModel { Id = "2", Title = "Short Warm Jacket", Price = 30, ImageUrl = "images/placeholders/270x295.svg" },
+				new BestSellersItemViewModel { Id = "3", Title = "Short Pocket Jacket", Price = 30, ImageUrl = "images/placeholders/270x295.svg" },
+				new BestSellersItemViewModel { Id = "4", Title = "Suit Pants", Price = 30, ImageUrl = "images/placeholders/270x295.svg" },
+				new BestSellersItemViewModel { Id = "5", Title = "Perfect Padded Jacket", Price = 30, ImageUrl = "images/placeholders/270x295.svg" },
+				new BestSellersItemViewModel { Id = "6", Title = "Knit Sweater", Price = 30, ImageUrl = "images/placeholders/270x295.svg" },
+				new BestSellersItemViewModel { Id = "7", Title = "Premium Boots", Price = 30, ImageUrl = "images/placeholders/270x295.svg" }
 			}
         };
 
