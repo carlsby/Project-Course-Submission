9031fded620e7db0b362e40990043cee836458ca
Gustav95 <gustav.eriksson@utb.ecutbildning.se>
2023-10-19 15:11:16 +0200

    telefon-verifieringen, AccountController, PhoneNumberViewModel

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,35 +1,59 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Project_Course_Submission.ViewModels;
 
 namespace Project_Course_Submission.Controllers
 {
-	public class AccountController : Controller
-	{
-		[Authorize]
-		public IActionResult Index()
-		{
-			return View();
-		}
+    public class AccountController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-
-		public IActionResult Verification()
-		{
-			return View();
-		}
 
         [Route("account/verification/otp")]
         public IActionResult OTP()
         {
             return View();
         }
-    }
-    
-  		public IActionResult NewPassword()
-		{
-			return View();
-		}
-	}
 
+        public IActionResult NewPassword()
+        {
+            return View();
+        }
+
+        public IActionResult Verification()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Verification(PhoneNumberViewModel phoneNumberViewModel)
+        {
+            string accountSid = "AC4857a3b575c88d6b68bc6c7bdccd844d";
+            string authToken = "b06a9b16ae197316f348b5e56c29536d";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            string fromPhoneNumber = "+13202868756";
+
+            string toPhoneNumber = phoneNumberViewModel.PhoneNumber!;
+            string messageBody = "hej";
+
+            MessageResource.Create(
+                body: messageBody,
+                from: new Twilio.Types.PhoneNumber(fromPhoneNumber),
+                to: new Twilio.Types.PhoneNumber(toPhoneNumber)
+            );
+
+            return RedirectToAction("OTP", "Account");
+        }
+
+    }
 }
 
-    
+
