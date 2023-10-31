abea4debc30851074e8f812e76e9a4a366ddb21d
Filippa Fast <filippa.fast@learnet.se>
2023-10-31 13:58:20 +0100

    Commiting before change branch - Filippa

--- Contexts/DataContext.cs
+++ Contexts/DataContext.cs
@@ -15,16 +15,18 @@ namespace Project_Course_Submission.Contexts
         public DataContext()
         {
         }
+        public DbSet<WishlistsEntity> Wishlist { get; set; }
+        public DbSet<OrderTrackEntity> Tracks { get; set; }
         public DbSet<CategoryEntity> Categories { get; set; }
+        public DbSet<CategoryImageEntity> CategoryImages { get; set; }
         public DbSet<ProductCategoryEntity> ProductCategories { get; set; }
         public DbSet<ProductImageEntity> ProductImages { get; set; }
         public DbSet<ImageEntity> Images { get; set; }
+        public DbSet<ProductReviewEntity> ProductReviews { get; set; }
         public DbSet<ReviewEntity> Reviews { get; set; }
         public DbSet<ProductTagEntity> ProductTags { get; set; }
         public DbSet<TagEntity> Tags { get; set; }
         public DbSet<ProductEntity> Products { get; set; }
-        public DbSet<WishlistsEntity> Wishlist { get; set; }
-        public DbSet<OrderTrackEntity> Tracks { get; set; }
         public DbSet<OrderEntity> Orders { get; set; }
 
     }
