797342f55a0ba0ba9a564bed05948db14449d837
Filippa Fast <filippa.fast@learnet.se>
2023-11-02 11:40:18 +0100

    Responsive Category icons & Best Sellers -> Done

---
+++ ViewModels/LatestProductsItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_Course_Submission.ViewModels
+{
+	public class LatestProductsItemViewModel
+	{
+		public int Id { get; set; }
+		public string ImageUrl { get; set; } = null!;
+		public string Title { get; set; } = null!;
+		public string ButtonUrl { get; set; } = null!;
+	}
+}
